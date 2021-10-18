7afaaf01a4f101a040849ccc5b63e9ab3e0f0342
vToruYamaguchi <tooru.yamaguchi2@veriserve.co.jp>
2021-10-18 13:44:40 +0900

    testcafe導入

---
+++ test/testcafe/test.js
@@ -0,0 +1,28 @@
+import { Selector } from "testcafe";
+
+// サイトの指定
+fixture("HOTEL PLANISPHERE").page(
+  "https://vtoruyamaguchi.github.io/hotel-example-site/ja/"
+);
+
+//テストケース1
+let testcase1 = "ログインテスト";
+test(testcase1, async (t) => {
+  //要素の取得
+  const loginButton = await Selector("#login-holder");
+  const email = await Selector("#email");
+  const password = await Selector("#password");
+  const login = await Selector("#login-button");
+
+  //テスト開始
+  await t
+    //それぞれの動作
+    .click(loginButton)
+    .typeText(email, "sakura@example.com")
+    .typeText(password, "pass1234")
+    .click(login);
+  //確認
+  await t
+    .expect(Selector("body > div > div:nth-child(1) > div > h2").innerText)
+    .eql("マイページ");
+});
